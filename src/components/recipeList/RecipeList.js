4c4db865625e2eeaaaebf0f1f9e92ade8a7af9a9
Andy <andyzanli@gmail.com>
2017-08-11 18:20:19 -0700

    Use beforeunload event listeners to handle local storage setting

--- src/components/recipeList/RecipeList.js
+++ src/components/recipeList/RecipeList.js
@@ -19,6 +19,8 @@ class RecipeList extends Component {
     this.addNewRecipe = this.addNewRecipe.bind(this);
     this.updateRecipe = this.updateRecipe.bind(this);
     this.deleteRecipe = this.deleteRecipe.bind(this);
+    this.componentStorageCleaup = this.componentStorageCleaup.bind(this);
+    this.active = false;
   }
 
   render() {
@@ -33,12 +35,13 @@ class RecipeList extends Component {
           :
 
           <div className="RecipeList-Container">
+            <div className="Title">Recipe Page</div>
             <Col className="center-block" sm={8}>
               <ListGroup componentClass="ul">
                 { this.state.list.length ?
                     this.state.list.map((item, idx) => {
                       if (item.name) {
-                        return <ListGroupItem key={idx} onClick={() => this.toggleDisplayRecipe(idx)}>{item.name}</ListGroupItem>;
+                        return <ListGroupItem key={idx} onClick={() => this.toggleDisplayRecipe(idx)} active={this[idx]}>{item.name}</ListGroupItem>;
                       } else {
                         return <RecipeInput key={idx} index={idx} recipe={this.state.list[idx - 1]} updateRecipe={this.updateRecipe} deleteRecipe={this.deleteRecipe} toggleDisplayRecipe={this.toggleDisplayRecipe}/>;
                       }
@@ -64,6 +67,25 @@ class RecipeList extends Component {
     }
   }
 
+  componentDidMount() {
+    window.addEventListener('beforeunload', this.componentStorageCleaup);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('beforeunload', this.componentStorageCleanup);
+  }
+
+  componentStorageCleaup() {
+    var stateList = this.state.list;
+    var storageList = [];
+    for (let i = 0; i < stateList.length; i++) {
+      if (!stateList[i].display) {
+        storageList.push(stateList[i]);
+      }
+    }
+    localStorage.setItem('list', JSON.stringify(storageList));
+  }
+
   toggleAddingRecipe() {
     this.setState({addingNewRecipe: !this.state.addingNewRecipe})
   }
@@ -71,8 +93,10 @@ class RecipeList extends Component {
   toggleDisplayRecipe(index) {
     var displayList = this.state.list;
     if (displayList[index + 1] && displayList[index + 1].display) {
+      this[index] = false;
       displayList.splice(index + 1, 1);
     } else {
+      this[index] = true;
       displayList.splice(index + 1, 0, {display: true});
     }
     this.setState({list: displayList});
@@ -89,21 +113,18 @@ class RecipeList extends Component {
     var updatedList = this.state.list;
     updatedList.splice(index, 1, recipe);
     this.setState({list: updatedList});
-    localStorage.setItem('list', JSON.stringify(updatedList));
   }
 
   deleteRecipe(index) {
     var updatedList = this.state.list;
     updatedList.splice(index, 2);
     this.setState({list: updatedList});
-    localStorage.setItem('list', JSON.stringify(updatedList));
   }
 
   addNewRecipe(recipe) {
     var updatedList = this.state.list.concat(recipe);
     this.setState({list: updatedList});
     this.toggleAddingRecipe();
-    localStorage.setItem('list', JSON.stringify(updatedList));
   }
 }
 
