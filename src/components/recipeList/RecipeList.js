cdcc703c1c2a319efc514ed11c2b0f1e641a47ef
Andy <andyzanli@gmail.com>
2017-08-11 17:36:45 -0700

    Save changes to localStorage

--- src/components/recipeList/RecipeList.js
+++ src/components/recipeList/RecipeList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './RecipeList.css';
 import RecipeInput from '../recipeInput/RecipeInput';
 // import ListItem from './ListItem';
-import { Row, Col, Button, FormGroup, FormControl, ControlLabel, ListGroup, ListGroupItem} from 'react-bootstrap';
+import { Col, Button, ListGroup, ListGroupItem} from 'react-bootstrap';
 
 
 class RecipeList extends Component {
@@ -15,7 +15,10 @@ class RecipeList extends Component {
       ],
     };
     this.toggleAddingRecipe = this.toggleAddingRecipe.bind(this);
+    this.toggleDisplayRecipe = this.toggleDisplayRecipe.bind(this);
     this.addNewRecipe = this.addNewRecipe.bind(this);
+    this.updateRecipe = this.updateRecipe.bind(this);
+    this.deleteRecipe = this.deleteRecipe.bind(this);
   }
 
   render() {
@@ -33,7 +36,13 @@ class RecipeList extends Component {
             <Col className="center-block" sm={8}>
               <ListGroup componentClass="ul">
                 { this.state.list.length ?
-                    this.state.list.map(item => <ListGroupItem>{item.name}</ListGroupItem>) :
+                    this.state.list.map((item, idx) => {
+                      if (item.name) {
+                        return <ListGroupItem key={idx} onClick={() => this.toggleDisplayRecipe(idx)}>{item.name}</ListGroupItem>;
+                      } else {
+                        return <RecipeInput key={idx} index={idx} recipe={this.state.list[idx - 1]} updateRecipe={this.updateRecipe} deleteRecipe={this.deleteRecipe} toggleDisplayRecipe={this.toggleDisplayRecipe}/>;
+                      }
+                    }) :
                     <ListGroupItem onClick={() => this.toggleAddingRecipe()}>{'Add a new recipe!'}</ListGroupItem> }
               </ListGroup>
             </Col>
@@ -55,19 +64,46 @@ class RecipeList extends Component {
     }
   }
 
-  componentWillUnmount() {
-    localStorage.setItem('list', JSON.stringify(this.state.list));
-  }
-
   toggleAddingRecipe() {
     this.setState({addingNewRecipe: !this.state.addingNewRecipe})
+  }
+
+  toggleDisplayRecipe(index) {
+    var displayList = this.state.list;
+    if (displayList[index + 1] && displayList[index + 1].display) {
+      displayList.splice(index + 1, 1);
+    } else {
+      displayList.splice(index + 1, 0, {display: true});
+    }
+    this.setState({list: displayList});
+  }
+
+  updateRecipe(recipe) {
+    var index;
+    for (let i = 0; i < this.state.list.length; i++) {
+      if (this.state.list[i].name === recipe.name) {
+        index = i;
+        break;
+      }
+    }
+    var updatedList = this.state.list;
+    updatedList.splice(index, 1, recipe);
+    this.setState({list: updatedList});
+    localStorage.setItem('list', JSON.stringify(updatedList));
+  }
+
+  deleteRecipe(index) {
+    var updatedList = this.state.list;
+    updatedList.splice(index, 2);
+    this.setState({list: updatedList});
+    localStorage.setItem('list', JSON.stringify(updatedList));
   }
 
   addNewRecipe(recipe) {
     var updatedList = this.state.list.concat(recipe);
     this.setState({list: updatedList});
-    console.log(updatedList, this.state.list);
     this.toggleAddingRecipe();
+    localStorage.setItem('list', JSON.stringify(updatedList));
   }
 }
 
