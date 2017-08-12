e3168a38aa470aecf5b2532c3067f3d3eec7a02d
Andy <andyzanli@gmail.com>
2017-08-11 17:35:59 -0700

    Save changes to localStorage

--- src/components/recipeInput/RecipeInput.js
+++ src/components/recipeInput/RecipeInput.js
@@ -6,71 +6,132 @@ class RecipeInput extends Component {
   constructor() {
     super();
     this.state = {
-      ingredientComponents: [
-        <FormControl key={1} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this.ingredient1 = ref}}/>,
-        <FormControl key={2} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this.ingredient2 = ref}}/>,
-        <FormControl key={3} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this.ingredient3 = ref}}/>,
-        <FormControl key={4} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this.ingredient4 = ref}}/>,
-      ],
+      ingredientComponents: [],
     };
-    this.rowKey = 5;
+    this.rowKey = 4;
     this.addIngredient = this.addIngredient.bind(this);
   }
 
   render() {
     return (
-      <div className="Recipe-Container">
-        <form>
+        this.props.recipe ?
 
-          <Row>
-            <Col className="center-block" sm={8}>
-              <FormGroup bsSize="large">
-                  <ControlLabel className="Recipe-Label">Recipe Name</ControlLabel>
-                  <FormControl type="text" autoFocus="true" componentClass="input" inputRef={(ref) => {this.recipeName = ref}}/>
-              </FormGroup>
-            </Col>
-          </Row>
+        <div className="Recipe-Container">
+          <form>
 
-          <Row>
-            <Col className="center-block" sm={8}>
+            <Row>
+              <Col sm={8}>
+                <FormGroup>
+                  <ControlLabel className="Recipe-Label">Ingredients</ControlLabel>
+                  {this.state.ingredientComponents}
+                </FormGroup>
+              </Col>
+            </Row>
+
+            <Row>
               <FormGroup>
-                <ControlLabel className="Recipe-Label">Ingredients</ControlLabel>
-                {this.state.ingredientComponents}
+                <Button className="Recipe-Save-Button" bsStyle="primary" type="submit" onClick={() => this.handleRecipeSave()}>Save Recipe</Button>
+                <Button className="Recipe-Delete-Button" onClick={() => this.handleRecipeDelete()}>Delete</Button>
+                <Button className="Recipe-Add-Button" bsSize="small" bsStyle="danger" onClick={this.addIngredient}> + </Button>
               </FormGroup>
-            </Col>
-          </Row>
+            </Row>
 
-          <Row>
-            <FormGroup>
-              <Button className="Save-Button" bsStyle="primary" type="submit" onClick={() => {
-                var newRecipe = {};
-                var ingredients = [];
-                newRecipe.name = this.recipeName.value;
-                for (let i = 1; i < this.rowKey; i++) {
-                  console.log(this['ingredient' + i].value)
-                  if (this['ingredient' + i].value) ingredients.push(this['ingredient' + i].value);
-                }
-                console.log('new recipe', newRecipe)
-                this.props.addNewRecipe(newRecipe);
-              }}>Save Recipe</Button>
-              <Button className="Delete-Button" onClick={() => this.handleDelete()}>Delete</Button>
-              <Button className="Add-Button" bsSize="small" bsStyle="danger" onClick={this.addIngredient}> + </Button>
-            </FormGroup>
-          </Row>
+          </form>
+        </div>
 
-        </form>
-      </div>
+        :
+
+        <div className="Empty-Container">
+          <form>
+
+            <Row>
+              <Col className="center-block" sm={8}>
+                <FormGroup bsSize="large">
+                    <ControlLabel className="Recipe-Label">Recipe Name</ControlLabel>
+                    <FormControl type="text" autoFocus="true" componentClass="input" inputRef={(ref) => {this.recipeName = ref}}/>
+                </FormGroup>
+              </Col>
+            </Row>
+
+            <Row>
+              <Col className="center-block" sm={8}>
+                <FormGroup>
+                  <ControlLabel className="Recipe-Label">Ingredients</ControlLabel>
+                  {this.state.ingredientComponents}
+                </FormGroup>
+              </Col>
+            </Row>
+
+            <Row>
+              <FormGroup>
+                <Button className="Empty-Save-Button" bsStyle="primary" type="submit" onClick={() => this.handleEmptySave()}>Save Recipe</Button>
+                <Button className="Empty-Delete-Button" onClick={() => this.handleEmptyDelete()}>Delete</Button>
+                <Button className="Empty-Add-Button" bsSize="small" bsStyle="danger" onClick={this.addIngredient}> + </Button>
+              </FormGroup>
+            </Row>
+
+          </form>
+        </div>
     );
   }
 
+  componentDidMount() {
+    if (this.props.recipe) {
+      var ingredientComponents = [];
+      this.rowKey = 0;
+      for (let i = 0; i < this.props.recipe.ingredients.length; i++) {
+        this.rowKey++;
+        ingredientComponents.push(<FormControl key={this.rowKey} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this['ingredient' + (i + 1)] = ref}} defaultValue={this.props.recipe.ingredients[i]}/>);
+        this.rowKey = i + 1;
+      }
+      this.setState({ingredientComponents});
+    } else {
+      this.setState({ingredientComponents:
+          [
+          <FormControl key={1} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this.ingredient1 = ref}}/>,
+          <FormControl key={2} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this.ingredient2 = ref}}/>,
+          <FormControl key={3} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this.ingredient3 = ref}}/>,
+          <FormControl key={4} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this.ingredient4 = ref}}/>,
+        ],
+      });
+    }
+  }
+
   addIngredient() {
-    var incrementedList = this.state.ingredientComponents.concat(<FormControl key={this.rowKey} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this['ingredient' + this.rowKey] = ref}}/>);
-    this.setState({ingredientComponents: incrementedList});
     this.rowKey++;
+    var incrementedList = this.state.ingredientComponents.concat(<FormControl key={this.rowKey} className="margin-bottom" type="text" componentClass="input" inputRef={(ref) => {this['ingredient' + this.rowKey] = ref;}}/>);
+    this.setState({ingredientComponents: incrementedList});
+  }
+
+  handleRecipeSave() {
+    var newRecipe = {};
+    var ingredients = [];
+    newRecipe.name = this.props.recipe.name;
+    for (let i = 1; i <= this.rowKey; i++) {
+      if (this['ingredient' + i].value) ingredients.push(this['ingredient' + i].value);
+    }
+    newRecipe.ingredients = ingredients;
+    this.props.updateRecipe(newRecipe);
+    this.props.toggleDisplayRecipe(this.props.index - 1);
+  }
+
+  handleRecipeDelete() {
+    this.props.deleteRecipe(this.props.index - 1)
+  }
+
+  handleEmptySave() {
+    var newRecipe = {};
+    var ingredients = [];
+    newRecipe.name = this.recipeName.value;
+    for (let i = 1; i <= this.rowKey; i++) {
+      if (this['ingredient' + i].value) ingredients.push(this['ingredient' + i].value);
+    }
+    newRecipe.ingredients = ingredients;
+    this.props.addNewRecipe(newRecipe);
   }
 
 
-  handleDelete() {
+  handleEmptyDelete() {
     this.props.toggleAddingRecipe();
   }
 }
