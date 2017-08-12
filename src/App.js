16df5babbdff9e4902ce79efd3b491c29de7c60e
Andy <andyzanli@gmail.com>
2017-08-11 17:33:52 -0700

    Removed RecipeInput

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import RecipeInput from './components/recipeInput/RecipeInput';
 import RecipeList from './components/recipeList/RecipeList';
 
 class App extends Component {
